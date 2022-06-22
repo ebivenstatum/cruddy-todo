2444c8ad5af08fe952e94b6f1de8faa1f9a3c5d9
chenyou <chenyouhuang@gmail.com>
2022-06-22 14:42:04 -0400

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -73,22 +73,37 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
-};
+  //var item = items[id];
+  //delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 
-// Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+        if (err) {
+          // report an error if item not found
+          callback(new Error(`No item with id: ${id}`));
+        } else {
+          callback();
+        }
+      });
+    }
+  });
+}
 
-exports.dataDir = path.join(__dirname, 'data');
+  // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
-exports.initialize = () => {
-  if (!fs.existsSync(exports.dataDir)) {
-    fs.mkdirSync(exports.dataDir);
-  }
-};
+  exports.dataDir = path.join(__dirname, 'data');
+
+  exports.initialize = () => {
+    if (!fs.existsSync(exports.dataDir)) {
+      fs.mkdirSync(exports.dataDir);
+    }
+  };
